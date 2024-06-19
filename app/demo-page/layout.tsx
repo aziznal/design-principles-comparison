e97e654e1b0d2171b53b88658b2f5418c268d84f
aziznal <aziznal.dev@gmail.com>
2024-06-19 13:14:33 +0300

    add back button from demo page

--- app/demo-page/layout.tsx
+++ app/demo-page/layout.tsx
@@ -1,12 +1,16 @@
 import { ModTool } from "@/components/ModTool";
 import { PropProvider } from "@/components/PropProvider";
 import { getDefaultProps } from "@/lib/default-props";
-import { Github } from "lucide-react";
+import { Github, LucideChevronLeft } from "lucide-react";
 import Link from "next/link";
 
 export default function DemoPageLayout({ children }: { children: React.ReactNode }) {
   return (
     <PropProvider props={getDefaultProps()}>
+      <Link href="/" className="fixed top-4 left-4 flex items-center gap-1">
+        <LucideChevronLeft /> Back
+      </Link>
+
       {children}
 
       <ModTool />
